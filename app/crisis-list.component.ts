0af25051f6540dda1c86b6871beed73d7ad2b404
rahulkesavan <b_k_rahul@yahoo.com>
2016-04-25 22:07:10 -0700

    Milestone1: update app.component.ts, add crisis-list.component.ts, hero-list.component.ts.

--- app/crisis-list.component.ts
+++ app/crisis-list.component.ts
@@ -2,8 +2,8 @@ import { Component } from 'angular2/core';
 
 @Component({
     template: `
-     <h2>CRISIS LIST</h2>
-     <span>Get crisis list here</span>
+     <h2>CRISIS CENTER</h2>
+     <span>Get your crisis here</span>
     `
 })
 export class CrisisListComponent {}
--- app/hero-list.component.ts
+++ app/hero-list.component.ts
@@ -3,7 +3,7 @@ import { Component } from 'angular2/core';
 @Component({
     template: `
      <h2>HEROS LIST</h2>
-     <span>Get heroes here</span>
+     <span>Get your heroes here</span>
     `
 })
 export class HeroListComponent {}
