e52eac63a07f0307da38d64521976720dc73d5e8
rahulkesavan <b_k_rahul@yahoo.com>
2016-04-25 21:56:30 -0700

    Add crisis-list.component.ts and hero-list.component.ts

---
+++ app/crisis-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    template: `
+     <h2>CRISIS LIST</h2>
+     <span>Get crisis list here</span>
+    `
+})
+export class CrisisListComponent {}
---
+++ app/hero-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    template: `
+     <h2>HEROS LIST</h2>
+     <span>Get heroes here</span>
+    `
+})
+export class HeroListComponent {}
